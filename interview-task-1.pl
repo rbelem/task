660cf082098180c7dd51b75cf4a4a5f9dac19608
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-03-06 13:04:56 -0400

    Store the csv data into an array of hashes to use it later on

--- interview-task-1.pl
+++ interview-task-1.pl
@@ -16,3 +16,8 @@ die("Can't read the input file '$input_filename'\n") unless -r $input_filename;
 die("The output file '$output_filename' already exists\n") unless !-e $output_filename;
 my $output_dir = dirname($output_filename);
 die("Can't write to the '$output_dir'\n") unless -w $output_dir;
+
+my @headers = qw(id product salesperson A B C D location departments E);
+
+my $input_csv = Text::CSV->new({ binary => 1, auto_diag => 1 });
+my $input_csv_aoh = $input_csv->csv(in => $input_filename, headers => [@headers]);
