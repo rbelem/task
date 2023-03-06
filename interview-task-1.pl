07467470f32042aabec474a787c120b5eed5ada2
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-03-06 09:33:07 -0400

    Add treatments for the cli arguments to avoid common errors

--- interview-task-1.pl
+++ interview-task-1.pl
@@ -2,7 +2,17 @@
 
 use strict;
 use warnings;
+use File::Basename;
 use Text::CSV;
 
 my $length = scalar @ARGV;
 die("Usage: interview-task-2.pl -i <input.csv> -o <output.csv>\n") unless ($length == 4 && $ARGV[0] eq "-i" && $ARGV[2] eq "-o");
+
+my $input_filename = $ARGV[1];
+my $output_filename = $ARGV[3];
+
+die("Can't locale the input file '$input_filename'\n") unless -e $input_filename;
+die("Can't read the input file '$input_filename'\n") unless -r $input_filename;
+die("The output file '$output_filename' already exists\n") unless !-e $output_filename;
+my $output_dir = dirname($output_filename);
+die("Can't write to the '$output_dir'\n") unless -w $output_dir;
